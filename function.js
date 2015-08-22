94cff34ee0824c83e25f4c96ccc1b981fc282ed5
tleifj <tleifj@traviss-mbp.attlocal.net>
2015-08-22 16:05:31 -0700

    Add smooth scrolling
    Add smooth scrolling to nav anchor links

--- function.js
+++ function.js
@@ -41,3 +41,21 @@ for (i=0; i < js.length; i++) {
 }
 
 // end hiding script from old browsers -->
+
+
+// Smooth scrolling for anchor tags
+
+$(function() {
+       $('a[href*=#]:not([href=#])').click(function() {
+         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+           var target = $(this.hash);
+           target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+               scrollTop: target.offset().top
+             }, 1000);
+             return false;
+           }
+         }
+       });
+     });
